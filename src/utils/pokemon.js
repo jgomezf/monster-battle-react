53421407f991b046de3048f079f4c6166b8e0fa2
Juan David Gomez <fogodaju@gmail.com>
2021-06-04 17:17:26 -0500

    Add utils

---
+++ src/utils/pokemon.js
@@ -0,0 +1,67 @@
+import $ from "jquery";
+
+export function choose(pokemon = {}) {
+  const {
+    name = "",
+    health: { initial = 0, current = 0, bar = "" },
+    types = [],
+    avatar = "",
+  } = pokemon;
+  const statusContainer = document.createElement("div");
+  $(statusContainer).addClass("status");
+
+  const typesArray = types.map((type) => {
+    return `<li class="button ${type.name}">${type.name}</li>`;
+  });
+
+  const typesString = typesArray.join("");
+
+  $(statusContainer).html(`
+    <div class="info">
+      <p class="name">Pokemon: ${name}</p>
+    </div>
+    <ul class="types">
+      ${typesString}
+    </ul>
+    <div class="meter">
+      <span class="bar ${bar}" style="width: 100%"></span>
+    </div>
+    <div class="health">${current}/${initial}</div>
+  `);
+
+  const pokemonContainer = document.createElement("div");
+  $(pokemonContainer).addClass("pokemon");
+
+  $(pokemonContainer).html(`<img src="${avatar}" />`);
+
+  return [statusContainer, pokemonContainer];
+}
+
+export function controls(pokemon = {}, message = "", callback) {
+  const { name = "", moves = [] } = pokemon;
+
+  const dialogContainer = document.createElement("div");
+  $(dialogContainer).addClass("dialog");
+  $(dialogContainer).html(`
+    <p>
+      <span class="name">${name}</span>,
+      <span class="message">${message}</span>
+    </p>
+  `);
+
+  const movesContainer = document.createElement("div");
+  $(movesContainer).addClass("moves");
+
+  moves.forEach((move) => {
+    const button = document.createElement("button");
+    $(button)
+      .addClass("button")
+      .addClass("black")
+      .text(move.name)
+      .click(callback);
+
+    $(movesContainer).append(button);
+  });
+
+  return [dialogContainer, movesContainer];
+}
